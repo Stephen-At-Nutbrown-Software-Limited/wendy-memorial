3ffe5dc1a201bb23426ce199826c3a1b12218210
Stephen-At-Nutbrown-Software-Limited <stephen@nutbrown.io>
2025-07-20 10:58:54 +0100

    Update birthday.js

--- static/js/birthday.js
+++ static/js/birthday.js
@@ -3,9 +3,9 @@
     var announcement = document.createElement('div');
     announcement.id = 'birthday-announcement';
     announcement.innerHTML =
-      "<p>We celebrate Wendy's birthday with grateful hearts.</p>" +
-      "<p><a href='#' id='extra-album-link'>See additional photo album</a></p>" +
-      "<p><em>Consider donating to St. Luke's in her honor.</em></p>";
+      "<p>We celebrate Wendy's birthday with grateful hearts and happy memories.</p>" +
+      "<p>More photos are available, <a href='#' id='extra-album-link'>see them here</a></p>" +
+      "<p><em>If you can, please <a href='https://www.stlukestribute.org.uk/initiatedonation/?firstName=Wendy&lastName=Nutbrown&imageUrl=/media/3731/mum.png&donationsFolderId=31267'>consider a donation to St Lukes in memory of Wendy.</a></em></p>";
     announcement.style.position = 'fixed';
     announcement.style.bottom = '0';
     announcement.style.left = '0';
