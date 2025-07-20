ca45946f70f8d23a64ad0760e742fcde8d63fed4
Stephen-At-Nutbrown-Software-Limited <stephen@nutbrown.io>
2025-07-20 11:27:25 +0100

    Move birthday announcement to top

--- public/js/birthday.js
+++ public/js/birthday.js
@@ -7,7 +7,7 @@
       "<p>More photos are available, <a href='#' id='extra-album-link'>see them here</a></p>" +
       "<p><em>If you can, please <a href='https://www.stlukestribute.org.uk/initiatedonation/?firstName=Wendy&lastName=Nutbrown&imageUrl=/media/3731/mum.png&donationsFolderId=31267'>consider a donation to St Lukes in memory of Wendy.</a></em></p>";
     announcement.style.position = 'fixed';
-    announcement.style.bottom = '0';
+    announcement.style.top = '0';
     announcement.style.left = '0';
     announcement.style.width = '100%';
     announcement.style.background = '#ffd';
