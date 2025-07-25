ece9bfd7ebb046130849eee07bd45877fe652884
Stephen Nutbrown <stephen@nutbrown.io>
2025-07-25 12:57:00 +0100

    Add Mum celebration album

--- public/js/birthday.js
+++ public/js/birthday.js
@@ -4,7 +4,7 @@
     announcement.id = 'birthday-announcement';
     announcement.innerHTML =
       "<p>We celebrate Wendy's birthday with grateful hearts and happy memories.</p>" +
-      "<p>More photos are available, <a href='#' id='extra-album-link'>see them here</a></p>" +
+      "<p>We've collected some special birthday photos together, <a href='https://photos.app.goo.gl/x6tC2n7D4ggVxqWB7' id='extra-album-link'>see them here</a></p>" +
       "<p><em>If you can, please <a href='https://www.stlukestribute.org.uk/initiatedonation/?firstName=Wendy&lastName=Nutbrown&imageUrl=/media/3731/mum.png&donationsFolderId=31267'>consider a donation to St Lukes in memory of Wendy.</a></em></p>";
     announcement.style.position = 'fixed';
     announcement.style.top = '0';
