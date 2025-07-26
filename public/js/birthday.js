50c265ef3ccc0705ac6654f83368e0858238f197
Stephen-At-Nutbrown-Software-Limited <stephen@nutbrown.io>
2025-07-26 11:59:56 +0100

    Make birthday banner appear above content

--- public/js/birthday.js
+++ public/js/birthday.js
@@ -6,7 +6,7 @@
       "<p>We celebrate Wendy's birthday with grateful hearts and happy memories.</p>" +
       "<p>We've collected some special birthday photos together, <a href='https://photos.app.goo.gl/x6tC2n7D4ggVxqWB7' id='extra-album-link'>see them here</a></p>" +
       "<p><em>If you can, please <a href='https://www.stlukestribute.org.uk/initiatedonation/?firstName=Wendy&lastName=Nutbrown&imageUrl=/media/3731/mum.png&donationsFolderId=31267'>consider a donation to St Lukes in memory of Wendy.</a></em></p>";
-    announcement.style.position = 'fixed';
+    announcement.style.position = 'sticky';
     announcement.style.top = '0';
     announcement.style.left = '0';
     announcement.style.right = '0';
@@ -18,7 +18,7 @@
     announcement.querySelectorAll('a').forEach(function(link) {
       link.style.textDecoration = 'underline';
     });
-    document.body.appendChild(announcement);
+    document.body.insertBefore(announcement, document.body.firstChild);
   }
 
   document.addEventListener('DOMContentLoaded', function() {
